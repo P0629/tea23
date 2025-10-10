d3e11509f7dac609d86d6102fae808fba759aa96
Peter Keßler <156343467+P0629@users.noreply.github.com>
2025-10-10 13:41:34 +0200

    Exercise-000 Teil 2&3

--- exercise-000/main.cpp
+++ exercise-000/main.cpp
@@ -2,6 +2,8 @@
 #include <fmt/format.h>
 
 #include "config.h"
+void changeValueByParameter(int value);
+void changeValueByPointer(int* ptr);
 
 int main(int argc, char **argv) {
 
@@ -15,7 +17,7 @@ int main(int argc, char **argv) {
     for (int i=0; i< argc; i++) {
         fmt::println("argc: {}, argument {} (Adresse: {})", i, argv[i], fmt::ptr(argv[i]));
     }
-    /*Teil 1*/
+    fmt::println("Teil 1");
     int foo = 10;
     int* ptr = &foo;
     *ptr = 42;
@@ -27,8 +29,21 @@ int main(int argc, char **argv) {
     *ptr = foo;     /*was steht in bar?*/
     fmt::print("bar = {}", bar);
 
-    /*Teil 2*/
-    
-
+    fmt::println("Teil 2");
+    double* d = new double(3.1415);   // Speicher reservieren UND initialisieren
+    fmt::println("Wert: {}", *d);    // Zugriff über Dereferenzierung
+    fmt::println("Adresse von d: {}", fmt::ptr(d));
+    delete d;                         // Speicher freigeben
     return 0; /* exit gracefully*/
+}
+
+void changeValueByParameter(int value) {
+    value = 4711;
+    fmt::println("Der Wert von value in der Funktion (changeValueByParameter): {}", value);
+}
+
+void changeValueByPointer(int* ptr) {
+    *ptr = 4711;
+    fmt::println("Der Wert von value in der Funktion (changeValueByPointer): {}", *ptr);
+    fmt::println("Die Adresse von ptr in der Funktion (changeValueByPointer): {}", fmt::ptr(ptr));
 }
