90f94e31c9989eeee9cade97964569201514f38e
Peter Keßler <156343467+P0629@users.noreply.github.com>
2025-10-10 12:47:56 +0200

    feat: exercise-000 Teil1

--- exercise-000/main.cpp
+++ exercise-000/main.cpp
@@ -11,10 +11,24 @@ int main(int argc, char **argv) {
      * More info at https://fmt.dev/latest/api.html
      */
     fmt::print("Hello, {} in version {}!\n", tea::PROJECT_NAME, tea::PROJECT_VER);
-
+/*Teil 0*/
     for (int i=0; i< argc; i++) {
         fmt::println("argc: {}, argument {} (Adresse: {})", i, argv[i], fmt::ptr(argv[i]));
     }
+    /*Teil 1*/
+    int foo = 10;
+    int* ptr = &foo;
+    *ptr = 42;
+    fmt::println("int foo = 10;");
+    fmt::println("int* ptr = &foo;");
+    fmt::println("Adresse von foo: {} Adresse im pointer ptr: {}", fmt::ptr(&foo), fmt::ptr(ptr));
+    int bar = 4711;
+    ptr = &bar;
+    *ptr = foo;     /*was steht in bar?*/
+    fmt::print("bar = {}", bar);
+
+    /*Teil 2*/
+    
 
     return 0; /* exit gracefully*/
 }
