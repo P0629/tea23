e3862b686188020c7a3062f0cc90db85731be877
Peter Keßler <156343467+P0629@users.noreply.github.com>
2025-10-17 15:32:00 +0200

    exercise-001 Teil 3

--- exercise-001/main.cpp
+++ exercise-001/main.cpp
@@ -24,13 +24,31 @@ int main(int argc, char **argv) {
     fmt::println("Wert: {}, Adresse: {}", globalVar, fmt::ptr(&globalVar));
     fmt::println("Wert: {}, Adresse: {}", globalVar2, fmt::ptr(&globalVar2));
     fmt::println("Wert: {}, Adresse: {}", globalVar3, fmt::ptr(&globalVar3));
-    fmt::print("Name: {}, Alter: {}\n Wert: {}, Adresse: {}", name, alter, fmt::ptr(&name), fmt::ptr(&alter));
+    fmt::println("Name: {}, Alter: {}\n Wert: {}, Adresse: {}", name, alter, fmt::ptr(&name), fmt::ptr(&alter));
     /*Die Speicheradresse ändert sich minimal*/
 
     /*Teil 2*/
     fmt::println("Teil 2");
+    int localVar = 2;
+    static int localVar2 = 2;
+    const int localVar3= 2;
+    fmt::println("Wert: {}, Adresse: {}", localVar, fmt::ptr(&localVar));
+    fmt::println("Wert: {}, Adresse: {}", localVar2, fmt::ptr(&localVar2));
+    fmt::println("Wert: {}, Adresse: {}", localVar3, fmt::ptr(&localVar3));
+    /*Adresse ändert sich gegenüber zu 1.4 und 1.5, da sie mit 0x7ff anfangen, die static int localVar2 startet gleich wie die globalen*/
 
+    /*Teil 3*/
+    fmt::println("Teil 3");
+    int* heapVar = new int(3);
+    fmt::println("Wert: {}, Adresse: {}", *heapVar, fmt::ptr(&heapVar));
+
+    delete heapVar;
+    /*Globale Variablen liegen im Datensegment und existieren während der gesamten Programmlaufzeit.
+Lokale Variablen liegen auf dem Stack und werden automatisch beim Funktionsaufruf angelegt und wieder gelöscht.
+Dynamisch erzeugte Variablen liegen auf dem Heap und müssen mit delete manuell freigegeben werden.
+static beschränkt die Sichtbarkeit, const macht den Wert unveränderlich.*/
+    
     /* INSERT YOUR CODE HERE */
 
     return 0; /* exit gracefully*/
-}
+} 
