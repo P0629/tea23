68253c848512574e03426de6f826c5d3dc136f3f
Peter Keßler <156343467+P0629@users.noreply.github.com>
2025-10-17 14:39:12 +0200

    exercise-001 Teil 1_1

--- exercise-001/main.cpp
+++ exercise-001/main.cpp
@@ -7,6 +7,8 @@
 int globalVar = 1;
 char name[] = "Peter";
 int alter = 25;
+const int globalVar2 = 1;
+static int globalVar3 =1;
 
 int main(int argc, char **argv) {
 
@@ -16,9 +18,18 @@ int main(int argc, char **argv) {
      * More info at https://fmt.dev/latest/api.html
      */
     fmt::print("Hello, {}!\n", tea::PROJECT_NAME);
+
+    /*Teil 1*/
     fmt::println("Teil 1");
     fmt::println("Wert: {}, Adresse: {}", globalVar, fmt::ptr(&globalVar));
+    fmt::println("Wert: {}, Adresse: {}", globalVar2, fmt::ptr(&globalVar2));
+    fmt::println("Wert: {}, Adresse: {}", globalVar3, fmt::ptr(&globalVar3));
     fmt::print("Name: {}, Alter: {}\n Wert: {}, Adresse: {}", name, alter, fmt::ptr(&name), fmt::ptr(&alter));
+    /*Die Speicheradresse ändert sich minimal*/
+
+    /*Teil 2*/
+    fmt::println("Teil 2");
+
     /* INSERT YOUR CODE HERE */
 
     return 0; /* exit gracefully*/
