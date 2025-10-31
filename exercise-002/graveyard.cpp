24c052d230d57930237ba1b1e0b49b7a0e6d73cb
Peter Keßler <156343467+P0629@users.noreply.github.com>
2025-10-31 15:47:48 +0100

    add Aufgabe 5

---
+++ exercise-002/graveyard.cpp
@@ -0,0 +1,17 @@
+#include <fmt/format.h>
+
+
+// Globale Variable – lebt im Datensegment
+int undead = 13;
+
+void awaken() {
+    // Lokale Variable mit gleichem Namen
+    int undead = 666;
+    fmt::println("👁️  Die lokale Variable ruft: {}", undead);
+}
+
+void persistent() {
+    static int undead = 42;
+    undead++;
+    fmt::println("💀 Der statische Geist zählt weiter: {}", undead);
+}
