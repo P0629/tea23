b3b1f3f0d9c5f5c0d2a6e452084c9c50df00e77a
Peter Keßler <156343467+P0629@users.noreply.github.com>
2025-11-28 14:31:57 +0100

    feat: add the fixed exercise 2

--- exercise-005/list.cpp
+++ exercise-005/list.cpp
@@ -1,3 +1,145 @@
+// #include "list.hpp"
+
+// #include <stdio.h>
+// #include <stdlib.h>
+
+// #include <fmt/format.h>
+
+// // 2.1 Knotenverwaltung
+
+// ListNode_t* NewListNode(void) {
+//     fmt::println("in \"{}\"",__func__);
+
+//     ListNode_t* node = (ListNode_t*)malloc(sizeof(ListNode_t));
+//     if (!node) return NULL;
+
+//     node->data = 0;
+//     node->pNext = NULL;
+//     return NULL;
+// }
+
+// void FreeListNode(ListNode_t* elem) {
+//     fmt::println("in \"{}\"",__func__);
+//     free(elem);
+
+// }
+
+// // 2.2 Listenverwaltung
+
+// List_t* NewList(void) {
+//     fmt::println("in \"{}\"",__func__);
+
+//     List_t* list = (List_t*)malloc(sizeof(List_t));
+//     if (!list) return NULL;
+
+//     list->pHead = NULL;
+//     list->pTail = NULL;
+//     list->size = 0;
+
+//     return NULL;
+// }
+
+// void FreeList(List_t* list) {
+//     fmt::println("in \"{}\"",__func__);
+
+//     ListNode_t* n = list->pHead;
+//     while (n != NULL) {
+//         ListNode_t* next = n->pNext;
+//         FreeListNode(n);
+//         n = next;
+//     }
+
+//     free(list);
+// }
+
+
+// // 2.3 Einfügen
+
+// int InsertIntoLinkedList(List_t* list, ListNode_t* elem) {
+//     fmt::println("in \"{}\"",__func__);
+
+//     if (!list || !elem) return EXIT_FAILURE;
+
+//     elem->pNext = NULL;                // immer am Ende
+
+//     if (list->pTail == NULL) {
+//         // leere Liste
+//         list->pHead = elem;
+//         list->pTail = elem;
+//     } else {
+//         // nicht leere Liste
+//         list->pTail->pNext = elem;
+//         list->pTail = elem;
+//     }
+
+//     list->size++;
+//     return EXIT_SUCCESS;
+// }
+
+// int InsertIntoLinkedListAfterNode(List_t* list, ListNode_t* node /* the node we insert the element into */, ListNode_t* elem) {
+//     fmt::println("in \"{}\"",__func__);
+
+//     if (!list || !node || !elem) return EXIT_FAILURE;
+
+//     elem->pNext = node->pNext;
+//     node->pNext = elem;
+
+//     if (node == list->pTail)
+//         list->pTail = elem;
+
+//     list->size++;
+//     return EXIT_SUCCESS;
+// }
+
+// // 2.4 Entfernen
+
+// int RemoveFromList(List_t* list, ListNode_t* elem) {
+//     fmt::println("in \"{}\"",__func__);
+
+//     if (!list || !elem) return EXIT_FAILURE;
+//     if (!list->pHead) return EXIT_FAILURE;
+
+//     // Element ist pHead
+//     if (list->pHead == elem) {
+//         list->pHead = elem->pNext;
+//         if (elem == list->pTail) {
+//             list->pTail = NULL;
+//         }
+//         FreeListNode(elem);
+//         list->size--;
+//     return EXIT_SUCCESS;
+// }
+
+// // Vorgänger suchen
+//     ListNode_t* prev = list->pHead;
+//     while (prev->pNext != NULL && prev->pNext != elem) {
+//         prev = prev->pNext;
+//     }
+
+//     if (prev->pNext == NULL)
+//         return EXIT_FAILURE; // nicht gefunden
+
+//     prev->pNext = elem->pNext;
+
+//     if (elem == list->pTail)
+//         list->pTail = prev;
+
+//     FreeListNode(elem);
+//     list->size--;
+
+//     return EXIT_SUCCESS;
+// }
+
+// // 2.5 Traversieren
+
+// ListNode_t* GetNext(const List_t* list, ListNode_t* elem) {
+//     fmt::println("in \"{}\"",__func__);
+
+//     if (elem == NULL)
+//         return list->pHead;
+//     return NULL;
+// }
+
 #include "list.hpp"
 
 #include <stdio.h>
@@ -8,26 +150,26 @@
 // 2.1 Knotenverwaltung
 
 ListNode_t* NewListNode(void) {
-    fmt::println("in \"{}\"",__func__);
+    fmt::println("in \"{}\"", __func__);
 
     ListNode_t* node = (ListNode_t*)malloc(sizeof(ListNode_t));
     if (!node) return NULL;
 
     node->data = 0;
     node->pNext = NULL;
-    return NULL;
+    return node;         // <-- fix: return the allocated node
 }
 
 void FreeListNode(ListNode_t* elem) {
-    fmt::println("in \"{}\"",__func__);
+    fmt::println("in \"{}\"", __func__);
+    if (!elem) return;
     free(elem);
-
 }
 
 // 2.2 Listenverwaltung
 
 List_t* NewList(void) {
-    fmt::println("in \"{}\"",__func__);
+    fmt::println("in \"{}\"", __func__);
 
     List_t* list = (List_t*)malloc(sizeof(List_t));
     if (!list) return NULL;
@@ -36,11 +178,12 @@ List_t* NewList(void) {
     list->pTail = NULL;
     list->size = 0;
 
-    return NULL;
+    return list;         // <-- fix: return the allocated list
 }
 
 void FreeList(List_t* list) {
-    fmt::println("in \"{}\"",__func__);
+    fmt::println("in \"{}\"", __func__);
+    if (!list) return;
 
     ListNode_t* n = list->pHead;
     while (n != NULL) {
@@ -52,11 +195,10 @@ void FreeList(List_t* list) {
     free(list);
 }
 
-
 // 2.3 Einfügen
 
 int InsertIntoLinkedList(List_t* list, ListNode_t* elem) {
-    fmt::println("in \"{}\"",__func__);
+    fmt::println("in \"{}\"", __func__);
 
     if (!list || !elem) return EXIT_FAILURE;
 
@@ -77,7 +219,7 @@ int InsertIntoLinkedList(List_t* list, ListNode_t* elem) {
 }
 
 int InsertIntoLinkedListAfterNode(List_t* list, ListNode_t* node /* the node we insert the element into */, ListNode_t* elem) {
-    fmt::println("in \"{}\"",__func__);
+    fmt::println("in \"{}\"", __func__);
 
     if (!list || !node || !elem) return EXIT_FAILURE;
 
@@ -94,7 +236,7 @@ int InsertIntoLinkedListAfterNode(List_t* list, ListNode_t* node /* the node we 
 // 2.4 Entfernen
 
 int RemoveFromList(List_t* list, ListNode_t* elem) {
-    fmt::println("in \"{}\"",__func__);
+    fmt::println("in \"{}\"", __func__);
 
     if (!list || !elem) return EXIT_FAILURE;
     if (!list->pHead) return EXIT_FAILURE;
@@ -106,11 +248,11 @@ int RemoveFromList(List_t* list, ListNode_t* elem) {
             list->pTail = NULL;
         }
         FreeListNode(elem);
-        list->size--;
-    return EXIT_SUCCESS;
-}
+        if (list->size > 0) list->size--;
+        return EXIT_SUCCESS;
+    }
 
-// Vorgänger suchen
+    // Vorgänger suchen
     ListNode_t* prev = list->pHead;
     while (prev->pNext != NULL && prev->pNext != elem) {
         prev = prev->pNext;
@@ -125,17 +267,18 @@ int RemoveFromList(List_t* list, ListNode_t* elem) {
         list->pTail = prev;
 
     FreeListNode(elem);
-    list->size--;
-
+    if (list->size > 0) list->size--;
     return EXIT_SUCCESS;
 }
 
 // 2.5 Traversieren
 
 ListNode_t* GetNext(const List_t* list, ListNode_t* elem) {
-    fmt::println("in \"{}\"",__func__);
+    fmt::println("in \"{}\"", __func__);
+    if (!list) return NULL;
 
     if (elem == NULL)
         return list->pHead;
-    return NULL;
+
+    return elem->pNext;    // <-- fix: return next element
 }
--- exercise-005/main.cpp
+++ exercise-005/main.cpp
@@ -20,10 +20,10 @@ auto main(int argc, char** argv) -> int
 
     ListNode_t* node = NewListNode();
     ListNode_t* elem = NewListNode();
-    FreeListNode(node);
+    // FreeListNode(node);
 
     List_t* list =  NewList();
-    FreeList(list);
+    // FreeList(list);
 
     int ret = EXIT_FAILURE;
     ret = InsertIntoLinkedList(list, node);
@@ -31,9 +31,56 @@ auto main(int argc, char** argv) -> int
 
      ret = InsertIntoLinkedListAfterNode(list, node, elem);
     fmt::println("Return value of: {}",ret);
+
     ret = RemoveFromList(list, elem);
     fmt::println("Return value of: {}",ret);
     ListNode_t* node2 =  GetNext(list, elem);
+
+
+    // =       AB HIER: 4.1 Test der Liste (minimaler Block)      =
+
+    fmt::println("\n===== Aufgabe 4.1: Liste testen =====");
+
+    List_t* myList = NewList();
+
+    // 5 Knoten erzeugen
+    ListNode_t* n1 = NewListNode();
+    ListNode_t* n2 = NewListNode();
+    ListNode_t* n3 = NewListNode();
+    ListNode_t* n4 = NewListNode();
+    ListNode_t* n5 = NewListNode();
+
+    // Werte zuweisen
+    n1->data = 10;
+    n2->data = 20;
+    n3->data = 30;
+    n4->data = 40;
+    n5->data = 50;
+
+    // Einfügen am Ende
+    InsertIntoLinkedList(myList, n1);
+    InsertIntoLinkedList(myList, n2);
+    InsertIntoLinkedList(myList, n3);
+    InsertIntoLinkedList(myList, n4);
+    InsertIntoLinkedList(myList, n5);
+
+    // Einfügen nach dem zweiten Knoten
+    ListNode_t* nAfter = NewListNode();
+    nAfter->data = 99;
+    InsertIntoLinkedListAfterNode(myList, n2, nAfter);
+
+    // Entfernen eines Elements (z. B. n4)
+    RemoveFromList(myList, n4);
+
+    // Traversieren und Ausgabe
+    fmt::println("Inhalt der Liste nach allen Operationen:");
+    for (ListNode_t* n = GetNext(myList, NULL); n != NULL; n = GetNext(myList, n)) {
+        fmt::print("{} ", n->data);
+    }
+    fmt::println("");
+
+    // Liste freigeben
+    FreeList(myList);
 
    
 
